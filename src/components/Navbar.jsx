705f47592a59fde3952947208ba4bdf3d17f7553
yas3jb <yasmu213@gmail.com>
2024-02-15 16:03:58 -0500

    Implement Navbar component with navigation links

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import puppy from "../images/puppy.png";
+
+export default function Navbar() {
+  return (
+    <div className="nav-container">
+      <img src={puppy} alt="image not found" />
+      <nav>
+        <ul>
+          <button className="details-btn">
+            <Link to="/">HOME</Link>
+          </button>
+          <button className="details-btn">
+            <Link to="/players">ROSTER</Link>
+          </button>
+        </ul>
+      </nav>
+    </div>
+  );
+}
