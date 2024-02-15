fd5e844dcfaf0cc671438603ed51f471bb120f3f
yas3jb <yasmu213@gmail.com>
2024-02-15 15:57:00 -0500

    Implement Home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h1>Welcome to Puppy Bowl</h1>
+      <p>
+        This is the home page of Puppy Bowl application. <br />
+        Please click the Roster link on the top right-hand corner.
+      </p>
+    </div>
+  );
+}
