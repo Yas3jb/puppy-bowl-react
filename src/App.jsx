617692831c05c15d11f7411272fe7f497588a8d9
yas3jb <yasmu213@gmail.com>
2024-02-15 16:05:37 -0500

    Added Navbar into App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import AllPlayers from "./components/AllPlayers";
 import Home from "./components/Home";
 import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/players" element={<AllPlayers />} />
